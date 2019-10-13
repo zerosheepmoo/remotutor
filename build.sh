6f31ea089089662ecf15009e5a983e33a290db0d
zerosheepmoo <zerosheepmoo@gmail.com>
2019-10-13 19:56:54 +0900

    deploy 2019-10-13 19:56:54

--- build.sh
+++ build.sh
@@ -1,29 +1,10 @@
 #!/usr/bin/env sh
 
 # abort on errors
-# set -e
+set -e
 
 # build
 npx vuepress build src --dest docs
-
-# navigate into the build output directory
-# cd docs <don't need to>
-
-# if you are deploying to a custom domain
-# echo '커스텀.도메인' > CNAME
-
-# git init
-# git add -A
-# git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-# git remote add origin https://github.com/onlydel/docs.wrw.kr.git
-# git push -f origin master:gh-pages
 
 # cd 
 
